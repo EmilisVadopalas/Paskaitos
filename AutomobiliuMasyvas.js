d5a1adc5a1fa277c338d17d32b20e8362c5751dd
Rokas Bundonis <RokasBundonis@gmail.com>
2022-03-05 13:45:01 +0200

    adding Cars

---
+++ AutomobiliuMasyvas.js
@@ -0,0 +1,27 @@
+arrayOfCars = [{
+    Marke: "Mercedes",
+    Modelis: "SL111",
+    Metai: 2015,
+    Spalva: "Pilka",
+    KuroSanaudos: 8.6,
+    Kuras: "Benzinas",
+}, {
+    Marke: "Audi",
+    Modelis: "A4",
+    Metai: 2018,
+    Spalva: "Melyna",
+    KuroSanaudos: 7.2,
+    Kuras: "Dyzelinas",
+    PriceToDrive: PriceToDriveMethod,
+    PriceToDriveObj: PriceToDriveObjMethod
+
+}, {
+    Marke: "Fiat",
+    Modelis: "500",
+    Metai: 2012,
+    Spalva: "Zalia",
+    KuroSanaudos: 5.9,
+    Kuras: "Benzinas",
+    PriceToDrive: PriceToDriveMethod,
+    PriceToDriveObj: PriceToDriveObjMethod
+}]
